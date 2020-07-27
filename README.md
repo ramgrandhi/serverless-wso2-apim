822933fe7a7bede1165380c2ad4b061d433c02cf
Ram Grandhi <ram.grandhi@icloud.com@users.noreply.github.com>
2020-07-27 22:37:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,91 @@
+serverless-wso2-apim
+====================
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Serverless Framework plugin to manage APIs in [WSO2 API Manager](https://wso2.com/api-management/).  
+
+
+## Compatible with
+* serverless@^1
+* [WSO2 API Manager 2.6.0](https://docs.wso2.com/display/AM260/)
+
+## Features
+* Create, Update APIs in WSO2 API Manager using `sls deploy apidefs`
+* View API deployment status using `sls list apidefs`
+* Delete APIs in WSO2 API Manager using `sls remove apidefs`
+* Automatically publish / re-publish APIs to WSO2 API Store when changes occur to API definitions
+
+## Install Plugin
+`yarn add -D serverless-wso2-apim`   
+OR   
+`npm install --save serverless-wso2-apim`  
+
+Then in `serverless.yml` add following entry to the plugins array:
+```yml
+plugins:
+  - serverless-wso2-apim
+```
+
+## Using the Plugin
+
+1. Add configuration options to your Serverless configuration, as below.
+```yml
+custom:
+  wso2apim:
+    host: 'wso2-apimanager.com' # WSO2 API Manager Host
+    port: 443 # WSO2 API Manager Port
+    versionSlug: 'v0.14'  # WSO2 API Manager's Management API version
+    user: 'user@tenant' # Username, with tenant symbol
+    pass: 'pass' # Password
+    gatewayEnv: 'Production'  # Target Gateway Environment
+```
+
+2. Add one or more API definitions to your Serverless configuration, as below.
+
+```yml
+apidefs:
+  - myAwesomeAPI: # Identifier of your API definition
+    name: 'MyAwesomeAPI'  # Name of API that shows up in WSO2 API Console (CANNOT BE UPDATED LATER)
+    description: 'My Awesome API'
+    rootContext: '/myawesomeapi'  # Runtime context of API which will be appended to the base URL exposed by WSO2 API Gateway. Must be unique across the Gateway Environment. (CANNOT BE UPDATED LATER)
+    version: 'v1' # Version, which also forms a part of the API URL ultimately (CANNOT BE UPDATED LATER)
+    visibility: 'PUBLIC'  # Accessible from Public Internet, Visible to everyone
+    backend: 
+      http: # HTTP-based Backends
+        baseUrl: 'https://backend-host:port/123'  # Backend RESTful base URL
+    maxTps: 100 # Throttling, Transactions per second
+    tags:
+      - my-awesome-api
+      - awesomeness
+    swaggerSpec:  # Swagger specification in YML, currently supports 2.0
+      swagger: '2.0'
+      ...
+      info:
+        ...
+      paths:
+        ...
+```
+
+3. Run `sls deploy apidefs` to create-and-publish or update-and-republish API definitions in WSO2 API Manager.
+
+4. Run `sls list apidefs` to view the status of API deployment on WSO2 API Manager.
+
+4. Run `sls remove apidefs` to delete API definitions from WSO2 API Manager when there are no active subscriptions made to those APIs.
+
+5. Run `sls remove apidefs --force` (** USE WITH CAUTION **) to forcefully delete API definitions despite any active subscriptions. It will retire, deprecate and delete API definitions if they cannot be deleted normally.
+
+
+## Limitations and Backlog items
+* Limited to creation of RESTful APIs using Swagger 2.0 on WSO2 API Manager. 
+* Limited to WSO2 API Manager 2.6.0 which uses v0.14 as management API version. 
+* Does not support managing APIs which are already created in another manner without using this Plugin.
+* Does not support custom certificates for backend endpoints.
+* Does not work with Websocket-style APIs.  
+* Does not support publishing API definitions to multiple Gateway environments.  
+* Does not support CORS configuration.
+
+## Help?
+* Raise an Issue 
+
+## License
+[MIT](https://github.com/99xt/serverless-dynamodb-local/blob/v1/LICENSE)
---
+++ index.js
@@ -0,0 +1,236 @@
+'use strict';
+const wso2apim = require("./src/2.6.0/wso2apim");
+
+console.log.apply(null);
+
+class ServerlessPlugin {
+
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+  
+    this.wso2APIM = serverless.service.custom.wso2apim;
+    this.apiDefs = serverless.pluginManager.serverlessConfigFile.apidefs;
+    this.cache = {}
+    this.cmd = this.serverless.pluginManager.cliCommands.join('|');
+
+
+    //Retrieve tenantSuffix in case of multi-tenant setup
+    if (this.wso2APIM.user.includes("@")) {
+      this.cache.tenantSuffix = this.wso2APIM.user.split("@")[1];
+    }
+
+    this.commands = {
+      deploy: {
+        commands: {
+          apidefs: {
+            usage: 'Creates or Updates API definitions in WSO2 API Manager',
+            lifecycleEvents: [
+              'registerClient',
+              'generateToken',
+              'createOrUpdateAPIDefs'
+            ],
+          }
+        },
+      },
+      list: {
+        commands: {
+          apidefs: {
+            usage: 'Lists the deployment status of API definitions in WSO2 API Manager',
+            lifecycleEvents: [
+              'registerClient',
+              'generateToken',
+              'listAPIDefs'
+            ]
+          }
+        }
+      },
+      remove: {
+        commands: {
+          apidefs: {
+            usage: 'Deletes API definitions in WSO2 API Manager',
+            lifecycleEvents: [
+              'registerClient',
+              'generateToken',
+              'removeAPIDefs'
+            ],
+          }
+        },
+      }
+    };
+
+
+    this.hooks = {
+      'deploy:apidefs:registerClient': this.registerClient.bind(this),
+      'deploy:apidefs:generateToken': this.generateToken.bind(this),
+      'deploy:apidefs:createOrUpdateAPIDefs': this.createOrUpdateAPIDefs.bind(this),
+      'list:apidefs:registerClient': this.registerClient.bind(this),
+      'list:apidefs:generateToken': this.generateToken.bind(this),
+      'list:apidefs:listAPIDefs': this.listAPIDefs.bind(this),
+      'remove:apidefs:registerClient': this.registerClient.bind(this),
+      'remove:apidefs:generateToken': this.generateToken.bind(this),
+      'remove:apidefs:removeAPIDefs': this.removeAPIDefs.bind(this)
+    };
+  };
+
+  async registerClient() {
+    try {
+      console.log("Registering client..");
+      const data = await wso2apim.registerClient(
+        "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/client-registration/" + this.wso2APIM.versionSlug + "/register",
+        this.wso2APIM.user,
+        this.wso2APIM.pass
+      );
+      this.cache.clientId = data.clientId;
+      this.cache.clientSecret = data.clientSecret;
+    }
+    catch(err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  }
+
+  async generateToken() {
+    try {
+      console.log("Generating temporary token..");
+      const data = await wso2apim.generateToken(
+        "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/oauth2/token",
+        this.wso2APIM.user,
+        this.wso2APIM.pass,
+        this.cache.clientId,
+        this.cache.clientSecret,
+        "apim:api_create apim:api_publish apim:api_view apim:subscribe apim:tier_view apim:tier_manage apim:subscription_view apim:subscription_block",
+      );
+      this.cache.accessToken = data.accessToken;
+    }
+    catch(err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  }
+
+  async listAPIDefs() {
+    if (this.cmd === 'list|apidefs')
+      console.log("Retrieving API Definitions..");
+    this.cache.deploymentStatus = [];
+    // Loops thru each api definition found in serverless configuration
+    for (let apiDef of this.apiDefs) {
+      try {
+        var apiDefClob = apiDef.name + "|-|" + apiDef.version + "|-|" + apiDef.rootContext;
+        const data = await wso2apim.isAPIDeployed(
+          "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+          this.cache.accessToken,
+          apiDef.name,
+          apiDef.version,
+          apiDef.rootContext
+        );
+
+        this.cache.deployedAPIs = [];
+        // Loops thru each deployed api definition returned from WSO2 API Manager (we need to find exact match out of 1:n results returned)
+        data.list.forEach(async (deployedAPI) => {
+          // Remove /t/* suffix if exists
+          if (deployedAPI.context.startsWith("/t/")) {
+            deployedAPI.context = deployedAPI.context.split(this.cache.tenantSuffix)[1]
+          }
+          this.cache.deployedAPIs.push({
+            apiId: deployedAPI.id,
+            apiClob: deployedAPI.name + "|-|" + deployedAPI.version + "|-|" + deployedAPI.context,
+            apiStatus: deployedAPI.status
+          });
+        });
+
+        // Compare apples-to-apples (configured-to-deployed) and record deployment status
+        if (this.cache.deployedAPIs.some(deployedAPI => deployedAPI.apiClob === apiDefClob)) {
+          this.cache.deploymentStatus.push({
+            apiName: apiDef.name,
+            apiVersion: apiDef.version,
+            apiContext: apiDef.rootContext,
+            apiStatus: this.cache.deployedAPIs.find(deployedAPI => deployedAPI.apiClob === apiDefClob).apiStatus,
+            apiId: this.cache.deployedAPIs.find(deployedAPI => deployedAPI.apiClob === apiDefClob).apiId,
+          });
+        }
+        else {
+          this.cache.deploymentStatus.push({
+            apiName: apiDef.name,
+            apiVersion: apiDef.version,
+            apiContext: apiDef.rootContext,
+            apiStatus: "TO BE CREATED",
+          })
+        }
+      }
+      catch (err) {
+        console.log(err);
+        throw new Error(err);
+      }
+      this.cache.deployedAPIs = [];
+    };
+    if (this.cmd === 'list|apidefs')
+      console.table(this.cache.deploymentStatus);
+  }
+
+  async createOrUpdateAPIDefs() {
+    try {
+      // By calling listAPIDefs(), we are re-collecting the current deployment status in WSO2 API Manager
+      await this.listAPIDefs();
+
+      for (const [i, api] of this.cache.deploymentStatus.entries()) {
+        try {
+          if (api.apiId !== undefined) {
+            console.log("Updating " + this.apiDefs[i].name + "(" + api.apiId + ")..");
+            const data = await wso2apim.updateAPIDef(
+              "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+              this.wso2APIM.user,
+              this.cache.accessToken,
+              this.wso2APIM.gatewayEnv,
+              this.apiDefs[i],
+              api.apiId
+            );
+          }
+          else {
+            console.log("Creating " + this.apiDefs[i].name + "..");
+            const data = await wso2apim.createAPIDef(
+              "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+              this.wso2APIM.user,
+              this.cache.accessToken,
+              this.wso2APIM.gatewayEnv,
+              this.apiDefs[i]
+            );
+          }
+        }
+        catch (err) {
+          console.log(err);
+        }
+      }
+    }
+    catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+    console.log("Deployment successful.", "\n", "Use `sls list apidefs` to view the deployment status.");
+  }
+
+  async removeAPIDefs() {
+    try {
+      // By calling listAPIDefs(), we are re-collecting the current deployment status in WSO2 API Manager
+      await this.listAPIDefs(); 
+      for (let api of this.cache.deploymentStatus) {
+        if (api.apiId.length > 0) {
+          console.log("Deleting " + api.apiId + "..");
+          const data = await wso2apim.removeAPIDef(
+            "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+            this.cache.accessToken,
+            api.apiId
+          );
+        }
+      };
+      console.log("Removal successful.", "\n", "Use `sls list apidefs` to view the deployment status.");
+    }
+    catch(err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  }
+
+}
+
+module.exports = ServerlessPlugin;
