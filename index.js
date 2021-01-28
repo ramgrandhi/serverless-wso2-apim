c5a28e25eb5fd9f9801b1c38791b40ba7ace20c5
Daniel Leu <Daniel.Leu@nn-group.com>
2021-01-28 20:49:05 +0100

    Add tests for the wso2apim module, refactor a bit

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-'use strict';
-const wso2apim = require("./src/2.6.0/wso2apim");
+const wso2apim = require('./src/2.6.0/wso2apim');
 
 console.log.apply(null);
 
 class ServerlessPlugin {
-
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-  
+
     this.wso2APIM = serverless.service.custom.wso2apim;
     this.apiDefs = serverless.pluginManager.serverlessConfigFile.apidefs;
-    this.cache = {}
+    this.cache = {};
     this.cmd = this.serverless.pluginManager.cliCommands.join('|');
 
-
     //Retrieve tenantSuffix in case of multi-tenant setup
-    if (this.wso2APIM.user.includes("@")) {
-      this.cache.tenantSuffix = this.wso2APIM.user.split("@")[1];
+    if (this.wso2APIM.user.includes('@')) {
+      this.cache.tenantSuffix = this.wso2APIM.user.split('@')[1];
     }
 
     this.commands = {
@@ -28,22 +25,19 @@ class ServerlessPlugin {
             lifecycleEvents: [
               'registerClient',
               'generateToken',
-              'createOrUpdateAPIDefs'
+              'createOrUpdateAPIDefs',
             ],
-          }
+          },
         },
       },
       list: {
         commands: {
           apidefs: {
-            usage: 'Lists the deployment status of API definitions in WSO2 API Manager',
-            lifecycleEvents: [
-              'registerClient',
-              'generateToken',
-              'listAPIDefs'
-            ]
-          }
-        }
+            usage:
+              'Lists the deployment status of API definitions in WSO2 API Manager',
+            lifecycleEvents: ['registerClient', 'generateToken', 'listAPIDefs'],
+          },
+        },
       },
       remove: {
         commands: {
@@ -52,39 +46,45 @@ class ServerlessPlugin {
             lifecycleEvents: [
               'registerClient',
               'generateToken',
-              'removeAPIDefs'
+              'removeAPIDefs',
             ],
-          }
+          },
         },
-      }
+      },
     };
-
 
     this.hooks = {
       'deploy:apidefs:registerClient': this.registerClient.bind(this),
       'deploy:apidefs:generateToken': this.generateToken.bind(this),
-      'deploy:apidefs:createOrUpdateAPIDefs': this.createOrUpdateAPIDefs.bind(this),
+      'deploy:apidefs:createOrUpdateAPIDefs': this.createOrUpdateAPIDefs.bind(
+        this
+      ),
       'list:apidefs:registerClient': this.registerClient.bind(this),
       'list:apidefs:generateToken': this.generateToken.bind(this),
       'list:apidefs:listAPIDefs': this.listAPIDefs.bind(this),
       'remove:apidefs:registerClient': this.registerClient.bind(this),
       'remove:apidefs:generateToken': this.generateToken.bind(this),
-      'remove:apidefs:removeAPIDefs': this.removeAPIDefs.bind(this)
+      'remove:apidefs:removeAPIDefs': this.removeAPIDefs.bind(this),
     };
-  };
+  }
 
   async registerClient() {
     try {
-      console.log("Registering client..");
+      console.log('Registering client..');
       const data = await wso2apim.registerClient(
-        "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/client-registration/" + this.wso2APIM.versionSlug + "/register",
+        'https://' +
+          this.wso2APIM.host +
+          ':' +
+          this.wso2APIM.port +
+          '/client-registration/' +
+          this.wso2APIM.versionSlug +
+          '/register',
         this.wso2APIM.user,
         this.wso2APIM.pass
       );
       this.cache.clientId = data.clientId;
       this.cache.clientSecret = data.clientSecret;
-    }
-    catch(err) {
+    } catch (err) {
       console.log(err);
       throw new Error(err);
     }
@@ -92,33 +92,41 @@ class ServerlessPlugin {
 
   async generateToken() {
     try {
-      console.log("Generating temporary token..");
+      console.log('Generating temporary token..');
       const data = await wso2apim.generateToken(
-        "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/oauth2/token",
+        'https://' +
+          this.wso2APIM.host +
+          ':' +
+          this.wso2APIM.port +
+          '/oauth2/token',
         this.wso2APIM.user,
         this.wso2APIM.pass,
         this.cache.clientId,
         this.cache.clientSecret,
-        "apim:api_create apim:api_publish apim:api_view apim:subscribe apim:tier_view apim:tier_manage apim:subscription_view apim:subscription_block",
+        'apim:api_create apim:api_publish apim:api_view apim:subscribe apim:tier_view apim:tier_manage apim:subscription_view apim:subscription_block'
       );
       this.cache.accessToken = data.accessToken;
-    }
-    catch(err) {
+    } catch (err) {
       console.log(err);
       throw new Error(err);
     }
   }
 
   async listAPIDefs() {
-    if (this.cmd === 'list|apidefs')
-      console.log("Retrieving API Definitions..");
+    if (this.cmd === 'list|apidefs') {
+      console.log('Retrieving API Definitions..');
+    }
+
     this.cache.deploymentStatus = [];
+    const { host, port, versionSlug } = this.wso2APIM;
+
     // Loops thru each api definition found in serverless configuration
     for (let apiDef of this.apiDefs) {
       try {
-        var apiDefClob = apiDef.name + "|-|" + apiDef.version + "|-|" + apiDef.rootContext;
+        var apiDefClob =
+          apiDef.name + '|-|' + apiDef.version + '|-|' + apiDef.rootContext;
         const data = await wso2apim.isAPIDeployed(
-          "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+          `https://${host}:${port}/api/am/publisher/${versionSlug}/apis`,
           this.cache.accessToken,
           apiDef.name,
           apiDef.version,
@@ -127,45 +135,82 @@ class ServerlessPlugin {
 
         this.cache.deployedAPIs = [];
         // Loops thru each deployed api definition returned from WSO2 API Manager (we need to find exact match out of 1:n results returned)
-        data.list.forEach(async (deployedAPI) => {
+        data.list.forEach((deployedAPI) => {
           // Remove /t/* suffix if exists
-          if (deployedAPI.context.startsWith("/t/")) {
-            deployedAPI.context = deployedAPI.context.split(this.cache.tenantSuffix)[1]
+          if (deployedAPI.context.startsWith('/t/')) {
+            deployedAPI.context = deployedAPI.context.split(
+              this.cache.tenantSuffix
+            )[1];
           }
           this.cache.deployedAPIs.push({
             apiId: deployedAPI.id,
-            apiClob: deployedAPI.name + "|-|" + deployedAPI.version + "|-|" + deployedAPI.context,
-            apiStatus: deployedAPI.status
+            apiClob:
+              deployedAPI.name +
+              '|-|' +
+              deployedAPI.version +
+              '|-|' +
+              deployedAPI.context,
+            apiStatus: deployedAPI.status,
           });
         });
 
         // Compare apples-to-apples (configured-to-deployed) and record deployment status
-        if (this.cache.deployedAPIs.some(deployedAPI => deployedAPI.apiClob === apiDefClob)) {
+        if (
+          this.cache.deployedAPIs.some(
+            (deployedAPI) => deployedAPI.apiClob === apiDefClob
+          )
+        ) {
           this.cache.deploymentStatus.push({
             apiName: apiDef.name,
             apiVersion: apiDef.version,
             apiContext: apiDef.rootContext,
-            apiStatus: this.cache.deployedAPIs.find(deployedAPI => deployedAPI.apiClob === apiDefClob).apiStatus,
-            apiId: this.cache.deployedAPIs.find(deployedAPI => deployedAPI.apiClob === apiDefClob).apiId,
+            apiStatus: this.cache.deployedAPIs.find(
+              (deployedAPI) => deployedAPI.apiClob === apiDefClob
+            ).apiStatus,
+            apiId: this.cache.deployedAPIs.find(
+              (deployedAPI) => deployedAPI.apiClob === apiDefClob
+            ).apiId,
+          });
+        } else {
+          this.cache.deploymentStatus.push({
+            apiName: apiDef.name,
+            apiVersion: apiDef.version,
+            apiContext: apiDef.rootContext,
+            apiStatus: 'TO BE CREATED',
           });
         }
-        else {
-          this.cache.deploymentStatus.push({
-            apiName: apiDef.name,
-            apiVersion: apiDef.version,
-            apiContext: apiDef.rootContext,
-            apiStatus: "TO BE CREATED",
-          })
-        }
-      }
-      catch (err) {
+      } catch (err) {
         console.log(err);
         throw new Error(err);
       }
       this.cache.deployedAPIs = [];
-    };
-    if (this.cmd === 'list|apidefs')
+    }
+
+    if (this.cmd === 'list|apidefs') {
       console.table(this.cache.deploymentStatus);
+    }
+  }
+
+  async getCreateOrUpdateApiDefMethod(apiDef, apiId) {
+    const { host, port, versionSlug, user, gatewayEnv } = this.wso2APIM;
+    const wso2Method =
+      apiId !== undefined ? wso2apim.updateAPIDef : wso2apim.createAPIDef;
+    const wso2Url = `https://${host}:${port}/api/am/publisher/${versionSlug}/apis`;
+    const logMessage =
+      apiId !== undefined
+        ? 'Updating ' + apiDef.name + '(' + apiId + ')..'
+        : 'Creating ' + apiDef.name + '..';
+
+    console.log(logMessage);
+
+    await wso2Method(
+      wso2Url,
+      user,
+      this.cache.accessToken,
+      gatewayEnv,
+      apiDef,
+      apiId
+    );
   }
 
   async createOrUpdateAPIDefs() {
@@ -173,64 +218,57 @@ class ServerlessPlugin {
       // By calling listAPIDefs(), we are re-collecting the current deployment status in WSO2 API Manager
       await this.listAPIDefs();
 
-      for (const [i, api] of this.cache.deploymentStatus.entries()) {
-        try {
-          if (api.apiId !== undefined) {
-            console.log("Updating " + this.apiDefs[i].name + "(" + api.apiId + ")..");
-            const data = await wso2apim.updateAPIDef(
-              "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
-              this.wso2APIM.user,
-              this.cache.accessToken,
-              this.wso2APIM.gatewayEnv,
-              this.apiDefs[i],
-              api.apiId
-            );
-          }
-          else {
-            console.log("Creating " + this.apiDefs[i].name + "..");
-            const data = await wso2apim.createAPIDef(
-              "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
-              this.wso2APIM.user,
-              this.cache.accessToken,
-              this.wso2APIM.gatewayEnv,
-              this.apiDefs[i]
-            );
-          }
-        }
-        catch (err) {
-          console.log(err);
-        }
-      }
-    }
-    catch (err) {
+      const mappedCreateOrUpdateAPIDefToRequests = this.cache.deploymentStatus
+        .entries()
+        .map(([i, { apiId }]) =>
+          this.getCreateOrUpdateApiDefMethod(this.apiDefs[i], apiId)
+        );
+
+      await Promise.all(mappedCreateOrUpdateAPIDefToRequests);
+    } catch (err) {
       console.log(err);
       throw new Error(err);
     }
-    console.log("Deployment successful.", "\n", "Use `sls list apidefs` to view the deployment status.");
+    console.log(
+      'Deployment successful.',
+      '\n',
+      'Use `sls list apidefs` to view the deployment status.'
+    );
   }
 
   async removeAPIDefs() {
     try {
       // By calling listAPIDefs(), we are re-collecting the current deployment status in WSO2 API Manager
-      await this.listAPIDefs(); 
-      for (let api of this.cache.deploymentStatus) {
-        if (api.apiId.length > 0) {
-          console.log("Deleting " + api.apiId + "..");
-          const data = await wso2apim.removeAPIDef(
-            "https://" + this.wso2APIM.host + ":" + this.wso2APIM.port + "/api/am/publisher/" + this.wso2APIM.versionSlug + "/apis",
+      await this.listAPIDefs();
+
+      const mappedRemoveApiDefsToRequests = this.cache.deploymentStatus
+        .filter((api) => api.apiId.length > 0)
+        .map((api) =>
+          wso2apim.removeAPIDef(
+            'https://' +
+              this.wso2APIM.host +
+              ':' +
+              this.wso2APIM.port +
+              '/api/am/publisher/' +
+              this.wso2APIM.versionSlug +
+              '/apis',
             this.cache.accessToken,
             api.apiId
-          );
-        }
-      };
-      console.log("Removal successful.", "\n", "Use `sls list apidefs` to view the deployment status.");
-    }
-    catch(err) {
+          )
+        );
+
+      await Promise.all(mappedRemoveApiDefsToRequests);
+
+      console.log(
+        'Removal successful.',
+        '\n',
+        'Use `sls list apidefs` to view the deployment status.'
+      );
+    } catch (err) {
       console.log(err);
       throw new Error(err);
     }
   }
-
 }
 
 module.exports = ServerlessPlugin;
--- src/2.6.0/wso2apim.js
+++ src/2.6.0/wso2apim.js
@@ -1,268 +1,229 @@
 const axios = require('axios');
 const https = require('https');
 const qs = require('qs');
-const path = require('path');
-const fs = require('fs');
-const swagger = require('swagger-client');
 
 function renderError(err) {
-    if (err.response) {
-        return({
-            response: err.response.data,
-            responseCode: err.response.status,
-            responseHeaders: err.response.headers,
-        });            
-    }
-    else if (err.request) {
-        return ({
-            request: err.request
-        });
-    }
-    else {
-        return ({
-            error: err.message
-        });
-    }
-};
-
+  if (err.response) {
+    return {
+      response: err.response.data,
+      responseCode: err.response.status,
+      responseHeaders: err.response.headers,
+    };
+  } else if (err.request) {
+    return {
+      request: err.request,
+    };
+  } else {
+    return {
+      error: err.message,
+    };
+  }
+}
 
 // Register a new client
 async function registerClient(url, user, pass) {
-    let authToken = user + ":" + pass;
-    let authTokenBase64 = new Buffer(authToken).toString('base64');
-    var data = {
-        "clientName": 'serverless-wso2-apim',
-        "owner": user,
-        "grantType": "password refresh_token",
-        "saasApp": true
-    };
-    var config = {
-        headers: {
-            'Authorization': 'Basic ' + authTokenBase64,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
+  let authToken = user + ':' + pass;
+  let authTokenBase64 = new Buffer(authToken).toString('base64');
+  var data = {
+    clientName: 'serverless-wso2-apim',
+    owner: user,
+    grantType: 'password refresh_token',
+    saasApp: true,
+  };
+  var config = {
+    headers: {
+      Authorization: 'Basic ' + authTokenBase64,
+      'Content-Type': 'application/json',
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-    return new Promise((resolve, reject) => {
-        axios.post(url, data, config)
-            .then((res) => {
-                resolve({
-                    clientId: res.data.clientId,
-                    clientSecret: res.data.clientSecret
-                });
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
+  try {
+    const { data: responseData } = await axios.post(url, data, config);
+    return {
+      clientId: responseData.clientId,
+      clientSecret: responseData.clientSecret,
+    };
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
 // Generate a new token
 async function generateToken(url, user, pass, clientId, clientSecret, scope) {
-    let authToken = clientId + ":" + clientSecret;
-    let authTokenBase64 = new Buffer(authToken).toString('base64');
-    var data = qs.stringify({
-        'grant_type': 'password',
-        'username': user,
-        'password': pass,
-        'scope': scope
-    });
-    var config = {
-        headers: {
-            'Authorization': 'Basic ' + authTokenBase64,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
+  let authToken = `${clientId}:${clientSecret}`;
+  let authTokenBase64 = new Buffer(authToken).toString('base64');
+  var data = qs.stringify({
+    grant_type: 'password',
+    username: user,
+    password: pass,
+    scope: scope,
+  });
+  var config = {
+    headers: {
+      Authorization: `Basic ${authTokenBase64}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-    return new Promise((resolve, reject) => {
-        axios.post(url, data, config)
-            .then((res) => {
-                resolve({
-                    accessToken: res.data.access_token
-                });
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
+  try {
+    const { data: responseData } = await axios.post(url, data, config);
+    return { accessToken: responseData.access_token };
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
+async function isAPIDeployed(
+  url,
+  accessToken,
+  apiName,
+  apiVersion,
+  apiContext
+) {
+  const queryStr = `query=name:${apiName} version:${apiVersion} context:${apiContext}`;
+  let config = {
+    headers: {
+      Authorization: 'Bearer ' + accessToken,
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-async function isAPIDeployed(url, accessToken, apiName, apiVersion, apiContext) {
-    let queryStr = "query=name:" + apiName + " version:" + apiVersion + " context:" + apiContext;
-    url = url + "?" + queryStr;
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
-
-    return new Promise((resolve, reject) => {
-        axios.get(url, config)
-            .then((res) => {
-                resolve(res.data);
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
-
+  try {
+    const { data } = await axios.get(`${url}?${queryStr}`, config);
+    return data;
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
 function constructAPIDef(user, gatewayEnv, apiDef, apiId) {
-    var wso2ApiDefinition = {};
+  var wso2ApiDefinition = {};
 
-    if (apiId !== undefined)
-        wso2ApiDefinition.id = apiId;
+  if (apiId !== undefined) wso2ApiDefinition.id = apiId;
 
-    wso2ApiDefinition = {
-        'name': apiDef.name,
-        'description': apiDef.description,
-        'context': apiDef.rootContext,
-        'version': apiDef.version,
-        'provider': user,
-        'apiDefinition': JSON.stringify(apiDef.swaggerSpec),
-        'status': 'CREATED',
-        'isDefaultVersion': false,
-        'type': 'HTTP',
-        'transport': ['https'],
-        'tags': [...apiDef.tags, "serverless-wso2-apim"],
-        'tiers': ['Unlimited'],
-        'maxTps': {
-            'sandbox': apiDef.maxTps,
-            'production': apiDef.maxTps
-        },
-        'visibility': apiDef.visibility,
-        'endpointConfig': JSON.stringify({
-            'production_endpoints': {
-                'url': apiDef.backend.http.baseUrl,
-                'config': null
-            },
-            'sandbox_endpoints': {
-                'url': apiDef.backend.http.baseUrl,
-                'config': null
-            },
-            'endpoint_type': 'http'
-        }),
-        'endpointSecurity': null,
-        'gatewayEnvironments': gatewayEnv,
-        'subscriptionAvailability': 'current_tenant',
-        'subscriptionAvailableTenants': [],
-        'businessInformation': {
-            'businessOwnerEmail': apiDef.swaggerSpec.info.contact.email,
-            'technicalOwnerEmail': apiDef.swaggerSpec.info.contact.email,
-            'technicalOwner': apiDef.swaggerSpec.info.contact.name,
-            'businessOwner': apiDef.swaggerSpec.info.contact.name
-        }
-    };
-    return wso2ApiDefinition;
-};
+  wso2ApiDefinition = {
+    name: apiDef.name,
+    description: apiDef.description,
+    context: apiDef.rootContext,
+    version: apiDef.version,
+    provider: user,
+    apiDefinition: JSON.stringify(apiDef.swaggerSpec),
+    status: 'CREATED',
+    isDefaultVersion: false,
+    type: 'HTTP',
+    transport: ['https'],
+    tags: [...apiDef.tags, 'serverless-wso2-apim'],
+    tiers: ['Unlimited'],
+    maxTps: {
+      sandbox: apiDef.maxTps,
+      production: apiDef.maxTps,
+    },
+    visibility: apiDef.visibility,
+    endpointConfig: JSON.stringify({
+      production_endpoints: {
+        url: apiDef.backend.http.baseUrl,
+        config: null,
+      },
+      sandbox_endpoints: {
+        url: apiDef.backend.http.baseUrl,
+        config: null,
+      },
+      endpoint_type: 'http',
+    }),
+    endpointSecurity: null,
+    gatewayEnvironments: gatewayEnv,
+    subscriptionAvailability: 'current_tenant',
+    subscriptionAvailableTenants: [],
+    businessInformation: {
+      businessOwnerEmail: apiDef.swaggerSpec.info.contact.email,
+      technicalOwnerEmail: apiDef.swaggerSpec.info.contact.email,
+      technicalOwner: apiDef.swaggerSpec.info.contact.name,
+      businessOwner: apiDef.swaggerSpec.info.contact.name,
+    },
+  };
+  return wso2ApiDefinition;
+}
 
 // Updates API definition
 async function updateAPIDef(url, user, accessToken, gatewayEnv, apiDef, apiId) {
-    url = url + "/" + apiId;
-    var data = constructAPIDef(user, gatewayEnv, apiDef, apiId);
-    var config = {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
+  url = url + '/' + apiId;
+  var data = constructAPIDef(user, gatewayEnv, apiDef, apiId);
+  var config = {
+    headers: {
+      Authorization: 'Bearer ' + accessToken,
+      'Content-Type': 'application/json',
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-    return new Promise((resolve, reject) => {
-        axios.put(url, data, config)
-            .then((res) => {
-                resolve(res.data);
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
-
+  try {
+    const { data: responseData } = await axios.put(url, data, config);
+    return responseData;
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
 // Creates API definition
 async function createAPIDef(url, user, accessToken, gatewayEnv, apiDef) {
-    var data = constructAPIDef(user, gatewayEnv, apiDef);
-    var config = {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
+  var data = constructAPIDef(user, gatewayEnv, apiDef);
+  var config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-    return new Promise((resolve, reject) => {
-        axios.post(url, data, config)
-            .then((res) => {
-                resolve({
-                    apiId: res.data.id,
-                    apiName: res.data.name,
-                    apiContext: res.data.context,
-                    apiStatus: res.data.status
-                });
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
+  try {
+    const { data: responseData } = await axios.post(url, data, config);
+    return {
+      apiId: responseData.id,
+      apiName: responseData.name,
+      apiContext: responseData.context,
+      apiStatus: responseData.status,
+    };
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
 async function removeAPIDef(url, accessToken, apiId) {
-    url = url + "/" + apiId;
-    let config = {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    };
+  url = `${url}/${apiId}`;
+  let config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  };
 
-    return new Promise((resolve, reject) => {
-        axios.delete(url, config)
-            .then((res) => {
-                console.log(res.data);
-                resolve(res.data);
-            })
-            .catch((err) => {
-                reject(
-                    renderError(err)
-                );
-            });
-    });
-};
-
+  try {
+    const { data: responseData } = await axios.delete(url, config);
+    return responseData;
+  } catch (error) {
+    throw renderError(error);
+  }
+}
 
 module.exports = {
-    registerClient,
-    generateToken,
-    isAPIDeployed,
-    createAPIDef,
-    updateAPIDef,
-    removeAPIDef
+  registerClient,
+  generateToken,
+  isAPIDeployed,
+  createAPIDef,
+  updateAPIDef,
+  removeAPIDef,
 };
